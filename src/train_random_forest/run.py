76aaa3f494cc392f7b520d2781b3a6120c94521b
Jake <fundsan@gmail.com>
2021-08-25 14:31:38 -0500

    added more to training

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -115,6 +115,7 @@ def go(args):
     # run.log_artifact to log the artifact to the run
     # YOUR CODE HERE
     ######################################
+
     artifact = wandb.Artifact(
             args.output_artifact,
             type="model_export",
@@ -134,7 +135,7 @@ def go(args):
     # Now log the variable "mae" under the key "mae".
     # YOUR CODE HERE
     ######################################
-
+    run.summary['mae'] = mae
     # Upload to W&B the feture importance visualization
     run.log(
         {
