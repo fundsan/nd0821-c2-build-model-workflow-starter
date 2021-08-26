1872fae6ca97c993acbcf9027e1cad785998e771
Jake <fundsan@gmail.com>
2021-08-26 11:57:36 -0500

    included lat & lon check in basic cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -43,6 +43,8 @@ def go(args):
     logger.info("Change last_review to datetime")
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
 
     logger.info("Logging artifact")
